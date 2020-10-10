18c0d51674a29aa8957ab537298a51e3d6ebc15b
Lokendra <lokilokendra12345@gmail.com>
2020-10-10 16:35:32 +0530

    {lokendra]Refactor.added a code to the uc_4_move_to_desired_location

--- workshop/Program.cs
+++ workshop/Program.cs
@@ -11,24 +11,31 @@ namespace TicTacToeGame
             TicTacToe t = new TicTacToe();
             char[] board = t.CreateBoard();
             char pLetter = t.ChooseLetter();
-            bool val = true;
-            while (val)
-            {
-                if (!(pLetter.Equals('X') || pLetter.Equals('O')))
-                {
-                    pLetter = t.ChooseLetter();
-                }
-                else
-                {
-                    val = false;
-                }
-            }
             char cLetter = 'X';
             if (pLetter.Equals('X'))
             {
                 cLetter = 'O';
             }
-            t.ShowBoard(board);
+            Console.WriteLine("Player's Letter = " + pLetter);
+            Console.WriteLine("Computer's Letter = " + cLetter);
+            t.PrintBoard(board);
+            bool playVal = true;
+            while (playVal)
+            {
+                Console.WriteLine("Choose a position among 1 to 9");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                bool check_if_empty = t.check_Availability(board, choice);
+                if (check_if_empty == false)
+                {
+                    Console.WriteLine("The position you chose is full, Please choose another position");
+                }
+                else
+                {
+                    board[choice] = pLetter;
+                    t.PrintBoard(board);
+                }
+               
+            }
         }
     }
 
@@ -48,21 +55,48 @@ namespace TicTacToeGame
         {
             Console.WriteLine("Choose a letter among X and O");
             char pLetter = Convert.ToChar(Console.ReadLine());
+            bool val = true;
+            while (val)
+            {
+                if (!(pLetter.Equals('X') || pLetter.Equals('O')))
+                {
+                    pLetter = ChooseLetter();
+                }
+                else
+                {
+                    val = false;
+                }
+            }
             return pLetter;
         }
 
-        public void ShowBoard(char[] board)
+        public void PrintBoard(char[] board)
         {
             for (int i = 1; i < 10;)
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    Console.Write(board[i] + "\t");
+                    Console.Write("|\t" + board[i] + "\t ");
                     i++;
                 }
                 Console.Write("\n");
+                Console.WriteLine("------------------------------------------------");
             }
         }
+
+        public bool check_Availability(char[] board, int k)
+        {
+            bool val = false;
+            if (board[k].Equals('0'))
+            {
+                val = true;
+            }
+            return val;
+        }
+
+
+
+
     }
 
 
