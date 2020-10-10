be305e5ca78d9ea419f018fb128b343ccf0498b1
Lokendra <lokilokendra12345@gmail.com>
2020-10-10 14:48:40 +0530

    {lokendra]Refactor.added a code to the uc_1_Create_Board

--- workshop/Program.cs
+++ workshop/Program.cs
@@ -4,9 +4,24 @@ namespace workshop
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Welcome to TictacToe Game");
+            TicTacToe t = new TicTacToe();
+            t.CreateBoard();
+        }
+    }
+
+    class TicTacToe
+    {
+        public void CreateBoard()
+        {
+            char[] board = new char[10];
+            for (int i = 1; i < 10; i++)
+            {
+                board[i] = Convert.ToChar(0);
+            }
         }
     }
 }
